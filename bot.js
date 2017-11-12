73151cf662cc6c917edd60b32bad9843568daa53
Z4F1_MacBook <10paspel@Pelle-Pastoor.local>
2017-11-12 21:40:11 +0100

    Update 0.0.1
    Connected to db, can get info from the db, start of point system

--- bot.js
+++ bot.js
@@ -1,3 +1,96 @@
-var bot = require("./bot-info/bot-info.js");
+var private = require("./private/info.js");
 
-console.log(bot.username);
+var tmi = require("tmi.js");
+
+var phrases = [
+    "Follow me to get notified the next time I'm live!",
+    "Like my stream and what I do? Then check out my youtube channel: https://www.youtube.com/channel/UCxI9wLt-MpuJkL0Ep6kIAKA"
+]
+
+var options = {
+    options: {
+        debug: true
+    },
+    connection: {
+        reconnect: true
+    },
+    identity: {
+        username: private.username,
+        password: private.password
+    },
+    channels: ["Z4F1_"]
+}
+
+var mysql = require("mysql");
+var private = require("./private/info.js");
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: private.mysqlPass,
+    database: "twitch"
+});
+
+con.connect(function(err){
+    if(err) throw err;
+    console.log("connected");
+});
+
+var bot = new tmi.client(options);
+
+bot.connect();
+
+bot.on("chat", function(channel, userstate, msg, self){
+    
+    if(self) return;
+    
+    if(msg.charAt(0) == "!") Command(userstate, msg.substring(1));
+});
+
+function Command(user, txt){
+    var command = txt.split(" ");
+    
+    switch(command[0]){
+        case "mods":
+            
+            bot.mods("Z4F1_").then(function(data){
+                var mods = "Current mods are: ";
+                for(var i = 0; i < data.length; i++){
+                    mods += data[i] + ", ";
+                }
+                
+                Say(mods);
+            });
+            
+            break;
+        case "points":
+            
+            con.query("SELECT * FROM viewers WHERE username='" + user.username + "'", function(err, result, fields){
+                if(err) throw err;
+                
+                console.log(result);
+                
+                Say("@" + user["display-name"] + " you have " + result[0]["points"] + " f*cking points man!");
+            });
+            
+            break;
+        default:
+            Say("Sry, didn't recognize that command! Try reading the description to see which commands you can use.");
+            break;
+    }
+}
+
+function Say(msg){
+    console.log("Just said: " + msg);
+    bot.say("Z4F1_", msg);
+}
+
+var index = 0; 
+setInterval(function(){
+    
+    Say(phrases[index]);
+    
+    index++;
+    if(index == phrases.length) index = 0;
+    
+}, 90000);
